f2281c96b5ad1fdc9ba8a965fe1c797f412559c2
eugene <eugene.s@nexttehnika.ru>
2021-01-19 15:12:31 +1000

    change first to console

--- first/src/main/java/org/next/test/spark/first/App.java
+++ first/src/main/java/org/next/test/spark/first/App.java
@@ -1,5 +1,6 @@
 package org.next.test.spark.first;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -8,28 +9,31 @@ import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
-import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
+import java.util.Map;
 
+@Slf4j
 public class App {
 
     public static final String INPUT_FILE = "data/first/example.txt";
-    public static final String OUTPUT_DIR = "data/first/example_result";
 
     public static void main(String[] args) throws Exception {
-        removeOldResult();
-        sparkStart(initSpark());
+        printLog(calc(initSpark()));
     }
 
-    private static void sparkStart(JavaSparkContext sc) {
+    private static void printLog(Map<String, Integer> calc) {
+        for (Map.Entry<String, Integer> stringIntegerEntry : calc.entrySet()) {
+            log.info("{} {}", stringIntegerEntry.getKey(), stringIntegerEntry.getValue());
+        }
+    }
+
+    private static Map<String, Integer> calc(JavaSparkContext sc) {
         JavaRDD<String> input = sc.textFile(INPUT_FILE);
         JavaRDD<String> words = input.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
         JavaPairRDD<String, Integer> counts = words
                 .mapToPair((PairFunction<String, String, Integer>) s -> new Tuple2<>(s, 1))
                 .reduceByKey((Function2<Integer, Integer, Integer>) (v1, v2) -> v1 + v2);
-        counts.saveAsTextFile(OUTPUT_DIR);
+        return counts.collectAsMap();
     }
 
     private static JavaSparkContext initSpark() {
@@ -38,25 +42,5 @@ public class App {
                 .setAppName("wordCount");
         JavaSparkContext sc = new JavaSparkContext(conf);
         return sc;
-    }
-
-    private static void removeOldResult() throws IOException {
-        Files.walkFileTree(Paths.get(OUTPUT_DIR),
-                new SimpleFileVisitor<Path>() {
-                    @Override
-                    public FileVisitResult postVisitDirectory(
-                            Path dir, IOException exc) throws IOException {
-                        Files.delete(dir);
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult visitFile(
-                            Path file, BasicFileAttributes attrs)
-                            throws IOException {
-                        Files.delete(file);
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
     }
 }
